3d297a2721c7172205dc23e34648959d89938daf
LinkolnR <lincolninsper@gmail.com>
2023-11-07 17:48:16 -0300

    atualizando para a leitura do .env

--- database.py
+++ database.py
@@ -1,10 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
+load_dotenv(override=True)
 
+host=os.getenv('MD_DB_SERVER')
+user=os.getenv('MD_DB_USERNAME')
+password=os.getenv('MD_DB_PASSWORD')
+database_name = "exemplo"
 
-# SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://username:password@ip/dbname"
+SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{user}:{password}@{host}/{database_name}"
 
 
 engine = create_engine(
