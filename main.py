98a14791e3bc5783c8fd9a5281e1cfab70193bbc
GiovanaCA <giovana.gca2002@gmail.com>
2023-11-07 22:13:24 -0300

    get planos finais e limpar comentarios

--- main.py
+++ main.py
@@ -124,52 +124,21 @@ def listar_personais_por_nome(membro_id: Annotated[int, Path(title="Identificado
     return personal_lista
 
 # ### GET PLANOS
-# @app.get("/plano/plano_id/{plano_id}/membros", response_model=list[Membro])
-# async def listar_membro_do_plano_id(plano_id: Annotated[int, Path(title="Identificador do plano",description="Coloque o identificador do plano para receber a lista dos membros que estão nesse plano", example=1)]):
-#     membros_lista = []
-#     planoId = 0
-#     for plano in planos:
-#         if plano_id == plano["plano_id"]:
-#             planoId = plano["plano_id"]
-#     if planoId != 0:
-#         membros_lista = filtra_e_devolve_lista_membros("plano_id",plano_id)
-#     else:
-#         detalhe = "Não existe um plano com esse id"
-#         raise HTTPException(status_code=400, detail=detalhe)
-#     if not membros_lista:
-#         detalhe = "Não existe um membro com esse plano"
-#         raise HTTPException(status_code=400, detail=detalhe)
-#     return membros_lista
+@app.get("/plano/plano_id/{plano_id}/membros", response_model=schemas.PlanoBase)
+def listar_membro_do_plano_id(plano_id: Annotated[int, Path(title="Identificador do plano",description="Coloque o identificador do plano para receber a lista dos membros que estão nesse plano", example=1)], db: Session = Depends(get_db)):
+    planos_lista = crud.get_plano_id_membros(db, plano_id)
+    if planos_lista is None:
+        detalhe = "Não existe um plano com esse id ou Não existe um membro com esse plano"
+        raise HTTPException(status_code=400, detail=detalhe,)
+    return planos_lista
 
-
-# @app.get("/plano/nome/{nome}/membros", response_model=list[Membro])
-# async def listar_membro_do_plano_nome(nome: Annotated[str, Path(title="Nome do plano",description="Coloque o nome do plano para receber a lista dos membros que estão nesse plano", example="basico")]):
-#     planoId = 0
-#     membros_lista = []
-#     for plano in planos:
-#         if nome.lower() == plano["nome"].lower():
-#             planoId = plano["plano_id"]
-#     if planoId != 0:
-#         membros_lista = filtra_e_devolve_lista_membros("plano_id",planoId)
-#     else:
-#         detalhe = "Não existe um plano com esse nome"
-#         raise HTTPException(status_code=400, detail=detalhe)
-#     if not membros_lista:
-#         detalhe = "Não existe um membro com esse plano"
-#         raise HTTPException(status_code=400, detail=detalhe)
-#     return membros_lista
-
-
-# @app.get("/plano/aulas_em_grupo", response_model=list[Plano])
-# async def listar_planos_com_aula_em_grupo():
-#     planos_list = []
-#     for plano in planos:
-#         if 1 == plano["aulas_em_grupo"]:
-#             planos_list.append(Plano(**plano))
-#     if not planos_list:
-#         detalhe = "Não há nenhuma aula em grupo :("
-#         raise HTTPException(status_code=400, detail=detalhe)
-#     return planos_list
+@app.get("/plano/nome/{nome}/membros", response_model=schemas.PlanoBase)
+def listar_membro_do_plano_nome(nome: Annotated[str, Path(title="Nome do plano",description="Coloque o nome do plano para ver as informações do plano escolhido", example="basico")], db: Session = Depends(get_db)):
+    planos_lista = crud.get_plano_nome_membros(db, nome)
+    if planos_lista is None:
+        detalhe = "Não existe um plano com esse nome ou Não existe um membro com esse plano"
+        raise HTTPException(status_code=400, detail=detalhe,)
+    return planos_lista
 
 @app.get("/plano/aulas_em_grupo", response_model=list[schemas.PlanoBase])
 def listar_planos_com_aulas_em_grupo(db: Session = Depends(get_db)):
@@ -179,14 +148,6 @@ def listar_planos_com_aulas_em_grupo(db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail=detalhe,)
     return planos_lista
 
-# @app.get("/plano/id/{plano_id}", response_model=Plano)
-# async def informacoes_plano_id(plano_id: Annotated[int, Path(title="Identificador do plano",description="Coloque o identificador do plano para ver as informações do plano escolhido", example=1)]):
-#     for plano in planos:
-#         if plano["plano_id"] == plano_id:
-#             return Plano(**plano)
-#     detalhe = "Não existe um plano com o esse id :("
-#     raise HTTPException(status_code=400, detail=detalhe)
-
 @app.get("/plano/id/{plano_id}", response_model=schemas.PlanoBase)
 def informacoes_plano_id(plano_id: Annotated[int, Path(title="Identificador do plano",description="Coloque o identificador do plano para ver as informações do plano escolhido", example=1)], db: Session = Depends(get_db)):
     planos_lista = crud.get_plano_id(db, plano_id)
@@ -195,15 +156,6 @@ def informacoes_plano_id(plano_id: Annotated[int, Path(title="Identificador do p
         raise HTTPException(status_code=400, detail=detalhe,)
     return planos_lista
 
-# @app.get("/plano/nome/{nome}", response_model=Plano)
-# async def infomacoes_plano_nome(nome: Annotated[str, Path(title="Nome do plano",description="Coloque o nome do plano para ver as informações do plano escolhido", example="basico")]):
-#     nome = nome.lower() 
-#     for plano in planos:
-#         if plano["nome"].lower() == nome:
-#             return Plano(**plano)
-#     detalhe = "Não existe nenhum plano com esse nome :("
-#     raise HTTPException(status_code=400, detail=detalhe)
-    
 @app.get("/plano/nome/{nome}", response_model=list[schemas.PlanoBase])
 def infomacoes_plano_nome(nome: Annotated[str, Path(title="Nome do plano",description="Coloque o nome do plano para ver as informações do plano escolhido", example="basico")], db: Session = Depends(get_db)):
     planos_lista = crud.get_plano_nome(db, nome)
@@ -211,17 +163,6 @@ def infomacoes_plano_nome(nome: Annotated[str, Path(title="Nome do plano",descri
         detalhe = "Não tem nenhum membro com esse nome"
         raise HTTPException(status_code=400, detail=detalhe,)
     return planos_lista
-
-# @app.get("/plano/promocao", response_model=list[Plano])
-# async def plano_promocao():
-#     planos_list = []
-#     for plano in planos:
-#         if plano["promocao"] == 1:
-#             planos_list.append(Plano(**plano))
-#     if  not planos_list:
-#         detalhe = "Nao tem nenhum palno com promocao :("
-#         raise HTTPException(status_code=400, detail=detalhe)
-#     return planos_list
 
 @app.get("/plano/promocao", response_model=list[schemas.PlanoBase])
 def listar_planos_com_promocao(db: Session = Depends(get_db)):
@@ -253,127 +194,15 @@ def deletar_plano(plano_id: Annotated[int, Path(title="Identificador do plano",d
     detalhe = "Não foi encontrado nenhum plano com esse id"
     raise HTTPException(status_code=400, detail=detalhe)
 
-# ### DELETES:
-# @app.delete("/membro/{membro_id}", response_model=list[Membro])
-# async def deletar_membro(membro_id: Annotated[int, Path(title="Identificador do membro",description="Coloque o identificador do membro para deletar o membro escolhido", example=1)]):
-#     membro_list = []
-#     membro_existe = None
-#     for membro in membros:
-#         if membro_id == membro["membro_id"]:
-#             membros.remove(membro)
-#             for personal in personais:
-#                 if membro_id in personal["membro_id"]:
-#                     personal["membro_id"].remove(membro_id)
-#             membro_existe = 1
-#             break
-#     if membro_existe is None:
-#         detalhe = "Não existe um membro com esse id para ser deletado"
-#         raise HTTPException(status_code=400, detail=detalhe)
-#     data["membro"] = membros
-#     data["personal"] = personais
-#     with open(file_json, "w") as arquivo:
-#         json.dump(data, arquivo, indent=4) 
-#     for membro in membros:
-#         membro_list.append(Membro(**membro))  
-#     return membro_list
-
-# @app.delete("/personal/{personal_id}",response_model=list[Personal])
-# async def deletar_personal(personal_id: Annotated[int, Path(title="Identificador do personal",description="Coloque o identificador do personal para deletar o personal escolhido", example=1)]):
-#     personal_list = []
-#     personal_existe = None
-#     for personal in personais:
-#         if personal_id == personal["personal_id"]:
-#             personais.remove(personal)
-#             for membro in membros:
-#                 if personal_id == membro["personal_id"]:
-#                     membro["personal_id"] = None 
-#             personal_existe = 1
-#     if personal_existe is None:
-#         detalhe = "Não existe um personal com esse id para ser deletado"
-#         raise HTTPException(status_code=400, detail=detalhe)
-#     data["personal"] = personais
-#     with open(file_json, "w") as arquivo:
-#         json.dump(data, arquivo, indent=4) 
-#     for personal in personais:
-#         personal_list.append(Personal(**personal))  
-#     return personais
-
-
-# @app.delete("/plano/{plano_id}")
-# async def deletar_plano(plano_id: Annotated[int, Path(title="Identificador do plano",description="Coloque o identificador do plano para deletar o plano escolhido", example=1)]):
-#     plano_existe = None
-#     for plano in planos:
-#         if plano_id == plano["plano_id"]:
-#             planos.remove(plano)
-#             for membro in membros:
-#                 if plano_id == membro["plano_id"]:
-#                     membro["plano_id"] = 0 # Caso o plano seja apagado, o usuário fica sem plano (plano =0) até entrar em outro
-#                     membro["ativo"] = 0
-#             plano_existe = 1
-#     if plano_existe is None:
-#         detalhe = "Não existe um plano com esse id para ser deletado"    
-#         raise HTTPException(status_code=400, detail=detalhe)
-#     data["plano"] = planos
-#     data["membro"] = membros
-#     with open(file_json, "w") as arquivo:
-#         json.dump(data, arquivo, indent=4) 
-#     planos_list  = []
-#     for plano in planos: 
-#         planos_list.append(Plano(**plano))
-#     return planos_list
-
-
 # # POSTS :
-
-# def serializar_datetime(obj):
-#     if isinstance(obj, date):
-#         return obj.isoformat()
-#     raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
-
-# def adicionar_membro_personal(membro_id,personal_id):
-#     for personal in personais:
-#         if personal["personal_id"] == personal_id:
-#             personal["membro_id"].append(membro_id)
-
-# def remover_membro_personal(membro_id,personal_id):
-#     for personal in personais:
-#         if personal["personal_id"] == personal_id:
-#             personal["membro_id"].remove(membro_id)
-
-# @app.post("/membro", status_code=201,response_model=Membro) 
-# async def adicionar_membro(membro: Annotated[Membro,Body(description="Corpo para envio das informações para serem adicionadas")]):
-#     membros.append(membro.dict())
-#     adicionar_membro_personal(membro.membro_id,membro.personal_id)
-#     data["membro"] = membros
-#     with open(file_json, "w") as arquivo:
-#         json.dump(data, arquivo, indent=4,default=serializar_datetime)  # indent=4 para formatar o JSON de forma legível
-#     return membro
-
 
 @app.post("/membro/", response_model=schemas.MembroBase)
 def create_user(membro: Annotated[schemas.MembroBase,Body(description="Corpo para envio das informações para serem adicionadas")], db: Session = Depends(get_db)):
     return crud.create_membro(db=db, membro=membro)
 
-
-# @app.post("/plano",status_code=201, response_model=Plano)
-# async def adicionar_plano(plano:Annotated[Plano,Body(description="Corpo para envio das informações para serem adicionadas")]):
-#     planos.append(plano.dict())
-#     data["plano"] = planos
-#     with open(file_json, "w") as arquivo:
-#         json.dump(data, arquivo, indent=4, default=serializar_datetime)  # indent=4 para formatar o JSON de forma legível
-#     return plano
-
 @app.post("/plano/", response_model=schemas.PlanoCreate)
 def create_plano(plano: Annotated[schemas.PlanoBase,Body(description="Corpo para envio das informações para serem adicionadas")], db: Session = Depends(get_db)):
     return crud.create_plano(db=db, plano=plano)
-
-# @app.post("/personal",status_code=201,response_model=Personal)
-# async def adicionar_personal(personal: Annotated[Personal,Body(description="Corpo para envio das informações para serem adicionadas")]):
-#     personais.append(personal.dict())
-#     data["personal"] = personais
-#     with open(file_json, "w") as arquivo:
-#         json.dump(data, arquivo, indent=4, default=serializar_datetime)  # indent=4 para formatar o JSON de forma legível
-#     return personal
 
 @app.post("/personal/", response_model=schemas.PersonalCreate)
 def create_personal(personal: Annotated[schemas.PersonalBase,Body(description="Corpo para envio das informações para serem adicionadas")], db: Session = Depends(get_db)):
@@ -389,7 +218,6 @@ def update_membro(membro_id: int, membro_update: schemas.MembroUpdate, db: Sessi
         raise HTTPException(status_code=404, detail="Membro não encontrado")
     return crud.get_membro_id(db=db,membro_id=membro_id)
 
-
 @app.put("/personal/{personal_id}", response_model=schemas.PersonalBase)
 def update_personal(personal_id: int, personal_update: schemas.PersonalUpdate, db: Session = Depends(get_db)):
     aux = crud.update_personal(personal_id=personal_id,db=db,personal_update=personal_update)
@@ -403,63 +231,3 @@ def update_plano(plano_id: int, plano_update: schemas.PlanoUpdate, db: Session =
     if aux is None:
         raise HTTPException(status_code=404, detail="plano não encontrado")
     return crud.get_plano_id(db=db,plano_id=plano_id)
-
-# @app.put("/membro/{membro_id}",response_model=MembroUpdate)
-# async def update_membro(membro_id: int, membro: Annotated[MembroUpdate,Body(description="Corpo para envio das informações a serem alteradas")]):
-#     membro_aux = membro.dict()
-#     membro_existe = None
-#     for memb in membros:
-#         if memb["membro_id"] == membro_id:
-#             remover_membro_personal(membro_id,memb["personal_id"])
-#             adicionar_membro_personal(membro_id,membro.personal_id)
-#             for chave, valor in membro_aux.items():
-#                 if valor is not None:
-#                     memb[chave] = valor
-#             membro_existe = 1
-#     if membro_existe is None:
-#         detalhe = "Não existe um membro com esse id para ser alterado"    
-#         raise HTTPException(status_code=400, detail=detalhe)
-#     data["membro"] = membros
-#     with open(file_json, "w") as arquivo:
-#         json.dump(data, arquivo, indent=4, default=serializar_datetime)  # indent=4 para formatar o JSON de forma legível
-#     return membro
-
-
-
-# @app.put("/personal/{personal_id}",response_model=PersonalUpdate)
-# async def update_personal(personal_id: int, personal: Annotated[PersonalUpdate,Body(description="Corpo para envio das informações a serem alteradas")]):
-#     personal_aux = personal.dict()
-#     personal_existe = None
-#     for pers in personais:
-#         if pers["personal_id"] == personal_id:
-#             for chave, valor in personal_aux.items():
-#                 if valor is not None:
-#                     pers[chave] = valor
-#             personal_existe = 1
-#     if personal_existe is None:
-#         detalhe = "Não existe um personal com esse id para ser alterado"    
-#         raise HTTPException(status_code=400, detail=detalhe)
-#     data["personal"] = personais
-#     with open(file_json, "w") as arquivo:
-#         json.dump(data, arquivo, indent=4, default=serializar_datetime)  # indent=4 para formatar o JSON de forma legível
-#     return personal
-
-
-
-# @app.put("/plano/{plano_id}",response_model=PlanoUpdate)
-# async def update_plano(plano_id: int, plano: Annotated[PlanoUpdate,Body(description="Corpo para envio das informações a serem alteradas")]):
-#     plano_aux = plano.dict()
-#     plano_existe = None
-#     for plan in planos:
-#         if plan["plano_id"] == plano_id:
-#             for chave, valor in plano_aux.items():
-#                 if valor is not None:
-#                     plan[chave] = valor
-#             plano_existe = 1
-#     if plano_existe is None:
-#         detalhe = "Não existe um plano com esse id para ser alterado"    
-#         raise HTTPException(status_code=400, detail=detalhe)
-#     data["plano"] = planos
-#     with open(file_json, "w") as arquivo:
-#         json.dump(data, arquivo, indent=4, default=serializar_datetime)  # indent=4 para formatar o JSON de forma legível
-#     return plano
